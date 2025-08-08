40f106c2710748f5685a4adbab617091ac734e6e
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-08-07 22:11:22 -0300

    feat: add category products page

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -3,34 +3,36 @@ import Link from "next/link";
 
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 
 interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  textContainerClassName?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   const firstVariant = product.variants[0];
-
-  // Proteção contra variant indefinido ou imageUrl inválido
-  const imageUrl = firstVariant?.imageUrl;
-
-  if (!imageUrl || typeof imageUrl !== "string" || !imageUrl.startsWith("http")) {
-    console.warn("Imagem inválida detectada:", imageUrl);
-    return null; // ou renderize um fallback de produto
-  }
-
   return (
-    <Link href="/" className="flex flex-col gap-4">
+    <Link
+      href={`/product-variant/${firstVariant.slug}`}
+      className="flex flex-col gap-4"
+    >
       <Image
-        src={imageUrl}
+        src={firstVariant.imageUrl}
         alt={firstVariant.name}
-        width={200}
-        height={200}
-        className="rounded-3xl"
+        sizes="100vw"
+        height={0}
+        width={0}
+        className="h-auto w-full rounded-3xl"
       />
-      <div className="flex max-w-[200px] flex-col gap-1">
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          textContainerClassName,
+        )}
+      >
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
@@ -42,6 +44,5 @@ const ProductItem = ({ product }: ProductItemProps) => {
     </Link>
   );
 };
-
 
 export default ProductItem;
